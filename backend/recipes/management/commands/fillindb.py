f320dca0241689add7cd1169d70805f57dae1829
zeffir4ik <nordisland@yandex.ru>
2022-06-24 22:31:56 +0300

    39

--- backend/recipes/management/commands/fillindb.py
+++ backend/recipes/management/commands/fillindb.py
@@ -3,6 +3,7 @@ import csv
 from django.core.management import BaseCommand
 
 from recipes.models import Ingredient
+from recipes.models import Tag
 
 
 class Command(BaseCommand):
@@ -12,10 +13,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         path = kwargs['path']
+        model = kwargs['model']
         with open(path, 'rt') as f:
             reader = csv.reader(f, delimiter=',')
-            for row in reader:
-                Ingredient.objects.get_or_create(
-                    name=row[0],
-                    measurement_unit=row[1]
-                )
+            if model == 'Ingredient':
+                for row in reader:
+                    Ingredient.objects.get_or_create(
+                        name=row[0],
+                        measurement_unit=row[1]
+                    )
+            if model == 'Tag':
+                for row in reader:
+                    Tag.objects.get_or_create(
+                        name=row[0],
+                        color=row[1],
+                        slug=row[2]
+                    )
